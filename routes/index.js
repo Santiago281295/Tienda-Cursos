0d7aec488f79ea0d7fc95c14b923d2ef182f1ef4
Santiago Martinez <josemartinez.1h@gmail.com>
2018-07-21 00:08:01 -0500

    Catalogo Productos

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/login', function(req, res, next) {
-  res.render('Back/login', { title: 'Express' });
-});
-
  /*Rutas Del Backend*/
 
 router.get('/Back', function(req, res, next) {
   res.render('Back/index', { title: 'Express' });
 });
+
+router.get('/Back/productos', function(req, res, next) {
+  res.render('Back/productos', { title: 'Express' });
+});
+
+
+
 
 module.exports = router;
