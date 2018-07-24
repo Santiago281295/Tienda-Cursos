2bf0b98068f7bef8adda5f740d7d19caa51bf63a
Adrian Alberto Cox Canche <street_cox1997@hotmail.com>
2018-07-24 02:55:59 -0500

    Conexión BD
    Conexión a la Base de Datos

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +22,26 @@ router.get('/usuarios', function(req, res, next) {
  /*Rutas Del Backend*/
 
 router.get('/Back', function(req, res, next) {
-  res.render('Back/index', { title: 'Express' });
+
+	db.query('SELECT * FROM usuarios', function(err, results) {
+		if (err){
+			console.log(err);
+
+			throw err;
+		}
+
+        for (var i = 0; i <= results.length - 1; i++) {
+        	console.log(results[0].IDUsuario);
+        	console.log(results[0].Nombre);
+        	console.log(results[0].Apellido);
+        	console.log(results[0].Correo);
+        	console.log(results[0].Contrasena);
+        }
+    })
+
+    db.end();
+
+	res.render('Back/index', { title: 'Express' });
 });
 
 router.get('/Back/productos', function(req, res, next) {
