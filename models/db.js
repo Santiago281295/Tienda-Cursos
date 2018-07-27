e14a2d4e00f218991a5528e3a8d1a4144a014c48
Adrian Alberto Cox Canche <street_cox1997@hotmail.com>
2018-07-27 18:23:45 -0500

    CRUD Usuarios y Manuales completos

--- models/db.js
+++ models/db.js
@@ -8,7 +8,23 @@ if (port == 4205) {
         user: 'root',
         password: '',
         database: 'proyectopaypal',
-        insecureAuth: true
+        insecureAuth: true,
+        // Con este atributo se Castean todos los campos del tipo BIT a BOOLEAN
+        typeCast: function castField(field, useDefaultTypeCasting ) {
+
+            // We only want to cast bit fields that have a single-bit in them. If the field
+            // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+            if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+                var bytes = field.buffer();
+
+                // A Buffer in Node represents a collection of 8-bit unsigned integers.
+                // Therefore, our single "bit field" comes back as the bits '0000 0001',
+                // which is equivalent to the number 1.
+                return( bytes[ 0 ] === 1 );
+            }
+
+            return( useDefaultTypeCasting() );
+        }
     });*/
     var connection = mysql.createConnection({
         host: 'cursosutm.mysql.database.azure.com',
@@ -16,7 +32,23 @@ if (port == 4205) {
         user: 'Santiago@cursosutm',
         password: 'Alpha281295@',
         database: 'proyectopaypal',
-        insecureAuth: true
+        insecureAuth: true,
+        // Con este atributo se Castean todos los campos del tipo BIT a BOOLEAN
+        typeCast: function castField(field, useDefaultTypeCasting ) {
+
+            // We only want to cast bit fields that have a single-bit in them. If the field
+            // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+            if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+                var bytes = field.buffer();
+
+                // A Buffer in Node represents a collection of 8-bit unsigned integers.
+                // Therefore, our single "bit field" comes back as the bits '0000 0001',
+                // which is equivalent to the number 1.
+                return( bytes[ 0 ] === 1 );
+            }
+
+            return( useDefaultTypeCasting() );
+        }
     });
 } else {
     console.log("No hay Conexión");
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ var db = require('../models/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  global.dirHost = req.get('host');
   res.render('index', { title: 'Wdemii-Inicio' });
 });
 
 router.get('/login', function(req, res, next) {
+  global.dirHost = req.get('host');
   if(global.logueado) {
     res.redirect('/');
   } else {
@@ -50,6 +52,7 @@ router.post('/login', function (req, res, next) {
 });
 
 router.get('/registro', function(req, res, next) {
+  global.dirHost = req.get('host');
   if (global.logueado) {
     res.redirect('/');
   } else {
@@ -104,6 +107,7 @@ router.post('/registro', function(req, res, next) {
 });
 
 router.get('/logout', function(req, res, next) {
+  global.dirHost = req.get('host');
   global.sessionerror = false;
   global.logueado = false;
   global.usuario = null;
@@ -112,6 +116,7 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.get('/carrito', function(req, res, next) {
+  global.dirHost = req.get('host');
   if (global.logueado) {
     res.render('carrito', { title: 'Wdemii-Carrito de Compras' });
   } else {
@@ -133,6 +138,8 @@ if (global.logueado) {
  */
 
 router.get('/Back', function(req, res, next) {
+  global.dirHost = req.get('host');
+  console.log(req);
 	if (global.logueado) {
     if (global.usuario == 'aurelio16.mex@gmail.com') {
       res.render('Back/index', { title: 'Wdemii-Administrador' });
@@ -145,6 +152,7 @@ router.get('/Back', function(req, res, next) {
 });
 
 router.get('/Back/cursos', function(req, res, next) {
+  global.dirHost = req.get('host');
   if (global.logueado) {
     if (global.usuario == 'aurelio16.mex@gmail.com') {
       db.query('SELECT * FROM productos WHERE Status=1', function(err, results) {
@@ -164,10 +172,175 @@ router.get('/Back/cursos', function(req, res, next) {
   }
 });
 
-router.get('/Back/cursos/nuevo', function (req, res, next) {
+router.get('/Back/curso_nuevo', function (req, res, next) {
+  global.dirHost = req.get('host');
   if (global.logueado) {
     if (global.usuario == 'aurelio16.mex@gmail.com') {
       res.render('Back/cursos/create', { title: 'Wdemii-Agregar Cursos' });
+    } else {
+      res.redirect('/');
+    }
+  } else {
+    res.redirect('/login');
+  }
+})
+
+router.get('/Back/usuarios', function(req, res, next) {
+  global.dirHost = req.get('host');
+  if (global.logueado) {
+    if (global.usuario == 'aurelio16.mex@gmail.com') {
+      db.query('SELECT * FROM usuarios WHERE Status=1', function(err, results) {
+        if (err){
+          console.log(err);
+
+          throw err;
+        }
+        res.render('Back/usuarios/index', { title: 'Wdemii-Gestión de Usuarios', queryUsuarios: results });
+      });
+    } else {
+      res.redirect('/');
+    }
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.get('/Back/usuario_nuevo', function (req, res, next) {
+  global.dirHost = req.get('host');
+  if (global.logueado) {
+    if (global.usuario == 'aurelio16.mex@gmail.com') {
+      res.render('Back/usuarios/create', { title: 'Wdemii-Agregar Usuarios' });
+    } else {
+      res.redirect('/');
+    }
+  } else {
+    res.redirect('/login');
+  }
+})
+
+router.post('/Back/usuario_nuevo', function (req, res, next) {
+  var nombre = req.body.txtNombre;
+  var apellido = req.body.txtApellido;
+  var correo = req.body.txtCorreo;
+  var contrasena = req.body.txtContrasena;
+  var fileName = '';
+  var pathFiles = './public/img/usuarios/profile_';
+  var fullPath = pathFiles + correo + '_';
+
+  if (req.files) {
+    try {
+      fileName = req.files.fileFoto.name;
+      let imgToSave = req.files.fileFoto;
+   
+      // Use the mv() method to place the file somewhere on your server
+      imgToSave.mv(fullPath + fileName, function(err) {
+        if (err) {
+          console.log('<imgError>');
+          console.log(err);
+          console.log('</imgError>');
+        }
+
+        console.log('Archivo guardado.');
+      });
+
+      fileName = 'profile_' + correo + '_' + fileName;
+    } catch(err) {
+      fileName = 'default.png';
+      console.log(err);
+    }
+  } else {
+    fileName = 'default.png';
+  }
+
+  db.query('INSERT INTO usuarios (Nombre, Apellido, Correo, Contrasena, Foto, TipoUsu, Cliente, Status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [nombre, apellido, correo, contrasena, fileName, 'Usuario', 0, 1], function(err, results) {
+    try {
+      res.redirect('/Back/usuarios');
+    } catch(err) {
+      res.redirect('/Back/usuario_nuevo');
+    }
+  });
+})
+
+router.get('/Back/usuario_editar/:id', function (req, res, next) {
+  global.dirHost = req.get('host');
+  if (global.logueado) {
+    if (global.usuario == 'aurelio16.mex@gmail.com') {
+      var id = req.params.id;
+
+      db.query('SELECT * FROM usuarios WHERE IDUsuario=? AND Status=1', id, function(err, results) {
+        if (err){
+          console.log(err);
+
+          throw err;
+        }
+        res.render('Back/usuarios/edit', { title: 'Wdemii-Editar Usuarios', queryEdit: results });
+      });
+    } else {
+      res.redirect('/');
+    }
+  } else {
+    res.redirect('/login');
+  }
+})
+
+router.post('/Back/usuario_editar/:id', function (req, res, next) {
+  var id = req.body.txtId;
+  var nombre = req.body.txtNombre;
+  var apellido = req.body.txtApellido;
+  var correo = req.body.txtCorreo;
+  var contrasena = req.body.txtContrasena;
+  var fileName = '';
+  var pathFiles = './public/img/usuarios/profile_';
+  var fullPath = pathFiles + correo + '_';
+
+  if (req.files) {
+    try {
+      fileName = req.files.fileFoto.name;
+      let imgToSave = req.files.fileFoto;
+   
+      // Use the mv() method to place the file somewhere on your server
+      imgToSave.mv(fullPath + fileName, function(err) {
+        if (err) {
+          console.log('<imgError>');
+          console.log(err);
+          console.log('</imgError>');
+        }
+
+        console.log('Archivo guardado.');
+      });
+
+      fileName = 'profile_' + correo + '_' + fileName;
+    } catch(err) {
+      fileName = req.body.txtFoto;
+      console.log(err);
+    }
+  } else {
+    fileName = req.body.txtFoto;
+  }
+
+  db.query('UPDATE usuarios set Nombre=?, Apellido=?, Correo=?, Contrasena=?, Foto=?, TipoUsu=?, Cliente=?, Status=? WHERE IDUsuario=?', [nombre, apellido, correo, contrasena, fileName, 'Usuario', 0, 1, id], function(err, results) {
+    try {
+      res.redirect('/Back/usuarios');
+    } catch(err) {
+      res.redirect('/Back/usuario_editar/' + id);
+    }
+  });
+})
+
+router.get('/Back/usuario_eliminar/:id', function(req, res, next) {
+  global.dirHost = req.get('host');
+  if (global.logueado) {
+    if (global.usuario == 'aurelio16.mex@gmail.com') {
+      var id = req.params.id;
+
+      db.query('UPDATE usuarios set Status=? WHERE IDUsuario=?', [0, id], function(err, results) {
+        if (err){
+          console.log(err);
+
+          throw err;
+        }
+        res.redirect('/Back/usuarios');
+      });
     } else {
       res.redirect('/');
     }
