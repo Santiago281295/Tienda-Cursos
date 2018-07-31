29d96d084fa9b7f158ea00887aa54461b81790d2
Adrian Alberto Cox Canche <street_cox1997@hotmail.com>
2018-07-31 03:14:03 -0500

    Nueva conexión de BD
    Agregué la conexión a la BD creada en GearHost para hacer pruebas.
    Cuando se solucione el error que está marcando el Azure se la cambiaré.

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
-port = process.env.PORT || 4205;
+// port = process.env.PORT || 4205;
 
-if (port == 4205) {
+// if (port == 4205) {
     /*var connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
@@ -26,7 +26,7 @@ if (port == 4205) {
             return( useDefaultTypeCasting() );
         }
     });*/
-    var connection = mysql.createConnection({
+    /*var connection = mysql.createConnection({
         host: 'cursosutm.mysql.database.azure.com',
         port: 3306,
         user: 'Santiago@cursosutm',
@@ -49,10 +49,34 @@ if (port == 4205) {
 
             return( useDefaultTypeCasting() );
         }
+    });*/
+    var connection = mysql.createConnection({
+        host: 'den1.mysql6.gear.host',
+        port: 3306,
+        user: 'tiendacursos',
+        password: 'Xa5883ZXT~i-',
+        database: 'tiendacursos',
+        insecureAuth: true,
+        // Con este atributo se Castean todos los campos del tipo BIT a BOOLEAN
+        typeCast: function castField(field, useDefaultTypeCasting ) {
+
+            // We only want to cast bit fields that have a single-bit in them. If the field
+            // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+            if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+                var bytes = field.buffer();
+
+                // A Buffer in Node represents a collection of 8-bit unsigned integers.
+                // Therefore, our single "bit field" comes back as the bits '0000 0001',
+                // which is equivalent to the number 1.
+                return( bytes[ 0 ] === 1 );
+            }
+
+            return( useDefaultTypeCasting() );
+        }
     });
-} else {
-    console.log("No hay Conexión");
-}
+// } else {
+//     console.log("No hay Conexión");
+// }
 
 connection.connect(function(err) {
     if (err) {
